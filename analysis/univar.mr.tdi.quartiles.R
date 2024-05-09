c0338e4ebb8e7724865ec73066e4fafe889ac333
Genevieve Leyden <rj18633@bristol.ac.uk>
2024-05-09 16:04:32 +0100

    script which ran univar mr by tdiQ and by season on the zhou vitD score

--- analysis/univar.mr.tdi.quartiles.R
+++ analysis/univar.mr.tdi.quartiles.R
@@ -40,13 +40,26 @@ extract_estimate <- function(summary) {
 ############### VARIABLES ###############
 ############### Vitamin D -> CRP 
 ## Data 
+
+### get all the input files 
+path <- getwd()
+##filenames <- list.files(path = path) ## the overall analysis is distinct analysis 
+f1 <- list.files(path = path, pattern = ".summer.txt")
+f2 <- list.files(path = path, pattern = ".winter.txt")
+filenames <- c(f1,f2)
+##names <- gsub(".txt.gz","",filenames)
+
+##q1 <- fread('pheno.q1.txt', header = T)
+
+##input <- 
 q1 <- fread('pheno.q1.txt', header = T)
 
-covs <- c("sex", "age.at.assessment", "assessmenth.centre", "month.of.assessment", ## keep month?? 
+
+covs <- c("sex", "age.at.assessment", "assessmenth.centre", "month.of.assessment", ## keep month?? ## makes no difference after checking
           "fasting.time", "chip", "PC1", "PC2", "PC3", "PC4", "PC5", "PC6", "PC7", 
           "PC8", "PC9", "PC10")
 
-iv <- "vit.d.score" ## Zhou score --> get it to loop through each of the scores 
+iv <- "vit.d.score" ## Update manually??! 
 
 exp <- "vitamin.d"
 
@@ -67,14 +80,33 @@ formula
 
 
 
+####################### Version 2 - loop 
 
-############### ANALYSIS ###############
-model <- ivreg(noquote(formula), data = q1) ## get it to loop through the datasets?! 
+results <- list()
 
-#summary(model, diagnostics = TRUE)
+# Iterate over each filename
+for (filename in filenames) {
+  # Read in the input file
+  q1 <- fread(filename, header = TRUE)  # Assuming data is in tabular format
+  
+  # Run the analysis
+  model <- ivreg(noquote(formula), data = q1)
+  
+  # Extract the estimates and weak instruments p-value
+  res <- extract_estimate(summary(model, diagnostics = TRUE))
+  
+  # Append results to the list
+  results[[filename]] <- res
+}
 
-res <- extract_estimate(summary(model, diagnostics = TRUE))
-print(res)
+# Combine all results into a single data frame
+combined_results <- do.call(rbind, results)
+
+combined_results$exp <- rownames(combined_results)
+row.names(combined_results) <- NULL
+combined_results$exp <- gsub("pheno\\.|\\.txt", "", combined_results$exp)
 
 
+##write.table(combined_results, "../../../../../../results/nlmr_vitd/univariable.mr.TDI/univar.tdiq.season.zhou.vitd.txt", col.names = T, row.names = F, quote = F, sep = "\t")
+  
 
