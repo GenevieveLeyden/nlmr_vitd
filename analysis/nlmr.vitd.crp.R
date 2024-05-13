4e6e9476aedd33754a9e30c13280f5b30c60c543
Genevieve Leyden <rj18633@bristol.ac.uk>
2024-05-13 12:08:22 +0100

    script used to generate main nlmr results - vitD-GWAS on CRP first

---
+++ analysis/nlmr.vitd.crp.R
@@ -0,0 +1,105 @@
+library(tidyverse)
+library(vroom)
+library(data.table)
+library(SUMnlmr)
+library(broom)
+library(patchwork)
+library(OneSampleMR)
+
+setwd('/Volumes/MRC-IEU-research/projects/ieu2/p1/131/working/data/nlmr_vitd/phenotype.dat/rap/working.data') 
+
+dat.c <- fread('pheno.dat.filtered.covars1.txt', header = T)
+
+
+### filter for compelte values 
+dat1 <- subset(dat.c, !is.na(crp))
+dat1 <- subset(dat1, !is.na(vitamin.d)) ## keeping only complete values of phenotype  ## N = 306,186
+dat1 <- subset(dat1, !is.na(fasting.time)) ## adding for completeness
+
+# y = measured outcome 
+# x = measured exposure 
+# g = genotype of exposure 
+
+####### 1. Residual method 
+
+##### a. Vit D GWAS score -> CRP 
+
+### i. create dummy matrix variable for covariates  
+dummies.1 <- model.matrix(~  sex + age.at.assessment + assessmenth.centre + month.of.assessment + fasting.time +
+                          PC1 + PC2 + PC3 + PC4 + PC5 + PC6 + PC7 + PC8 + PC9 + PC10, data = dat1)[,-1]
+
+NROW(dummies.1)
+
+### ii. create summary data 
+vitd.residual.1<-create_nlmr_summary(y = dat1$crp.log,
+                                      x = dat1$vitamin.d,
+                                      g = dat1$vit.d.score,
+                                      covar = dummies.1,
+                                      family = "gaussian",
+                                      strata_method = "residual", 
+                                      controlsonly = FALSE,
+                                      q = 10)
+
+head(vitd.residual.1)$summary
+
+
+### iii. fit the model
+model.residual.1<- with(vitd.residual.1$summary, frac_poly_summ_mr(bx=bx,
+                                                            by=by, 
+                                                            bxse=bxse, 
+                                                            byse=byse, 
+                                                            xmean=xmean,
+                                                            family="gaussian",
+                                                            fig=TRUE)
+)
+
+
+
+summary(model.residual.1)
+
+model.residual.1$lace  
+
+### iv. get the effect estimates for each strata 
+effs1 <-model.residual.1$lace %>% as.data.frame() %>% mutate(method = "Residual") %>%
+  mutate(strata = row_number())
+  
+
+
+
+####### 2. Doubly ranked method 
+
+##### a. Vit D GWAS score -> CRP 
+
+### i. creae the summary data - same covariates as before 
+vitd.residual.1<-create_nlmr_summary(y = dat1$crp.log,
+                                      x = dat1$vitamin.d,
+                                      g = dat1$vit.d.score,
+                                      covar = dummies.1,
+                                      family = "gaussian",
+                                      strata_method = "ranked", 
+                                      controlsonly = FALSE,
+                                      q = 10)
+
+head(vitd.residual.1)$summary
+
+
+model.ranked.1<- with(vitd.residual.1$summary, frac_poly_summ_mr(bx=bx,
+                                                            by=by, 
+                                                            bxse=bxse, 
+                                                            byse=byse, 
+                                                            xmean=xmean,
+                                                            family="gaussian",
+                                                            fig=TRUE)
+)
+
+
+
+summary(model.ranked.1)
+
+model.ranked.1$lace  
+model.ranked.1$p_tests
+model.ranked.1$p_heterogeneity
+
+
+effs2 <-model.ranked.1$lace %>% as.data.frame() %>% mutate(method = "Ranked") %>%
+  mutate(strata = row_number())
